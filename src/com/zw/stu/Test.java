786e3fe68f03216ae5310d1f175d6ef11fc0ded1
zhouwei <zhouwei11@58ganji.com>
2017-12-15 15:58:36 +0800

    first commit

---
+++ src/com/zw/stu/Test.java
@@ -0,0 +1,21 @@
+package com.zw.stu;
+import java.util.concurrent.*;
+public class Test {
+    private static ExecutorService executor = Executors.newSingleThreadExecutor();
+    public static void main(String[] args) {
+        ExecutorService threadPool = Executors.newSingleThreadExecutor();
+        Future<String> future = threadPool.submit(new Callable<String>() {
+            public String call() throws Exception {
+                Thread.sleep(2000);
+                System.out.println("call()");
+                return "Hello World";
+            }
+        });
+        try {
+            String s = future.get();
+            System.out.println(s + "=========");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
